9b0fcf390ccb7432b5edfc4909373388e31642d7
Db-Lau <debbielau.hk@gmail.com>
2025-09-29 13:32:45 +0200

    update dagster

---
+++ 15_orchestration_dagster_duckdb/orchestration/definitions.py
@@ -0,0 +1,18 @@
+# ==================== #
+#                      #
+#       imports        #
+#                      #
+# ==================== #
+
+import dlt
+import dagster as dg
+from dagster_dlt import DagsterDltResource, dlt_assets
+from dagster_dbt import DbtCliResource, dbt_assets, DbtProject
+
+# to import dlt script
+import sys
+sys.path.insert(0, '../data_extract_load')
+from load_job_ads import jobads_source 
+
+# data warehouse directory
+db_path = str(Path(__file__).parents[1] / "data_warehouse/job_ads.duckdb")
